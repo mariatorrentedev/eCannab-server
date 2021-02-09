a136bd2bb1613bf7d368f827fe7879e098db086a
María Torrente <caritotorrente@gmail.com>
2021-02-09 11:23:40 -0500

    fixing plural typo to singular

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -39,7 +39,7 @@ VALUES ('Multipurpose Oil 600mg Nano Cbd','Momentum 10X', 'http://bit.ly/momentu
 
 INSERT INTO
 
-resources (name, link)
+resources (name, link, user_id)
 
 VALUES ('Why CBD is great for your health?','http://bit.ly/resource_momentum', 1),
 ('Lab Analysis Pure CBD','http://bit.ly/spero_resource', 2),
