c9240fb3d362a6529c95fde369989a804ba16df4
María Torrente <caritotorrente@gmail.com>
2021-02-09 11:21:36 -0500

    fixing ' in seed file

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -23,26 +23,27 @@ VALUES (
 );
 
 INSERT INTO
-sites (brand, logo, seller_description, subdomain, user_id)
+sites (brand, logo, banner, seller_description, subdomain, user_id)
 
-VALUES ("Momentum 10x", "http://bit.ly/momentum_logo", "http://bit.ly/banner_yellow","We're a brand commited with the health and overall wellness!","momentum10x",1),
-("Spero", "http://bit.ly/spero_logo","http://bit.ly/spero_banner","Spero uses non-GMO, organically-produced hemp harvested only from – farmsright here in the U.S. of A for sustain nably-made products you can trust.","spero",2),
-("XTEND5", "http://bit.ly/xtend5_logo", "http://bit.ly/banner_xtend5","We're a brand commited with the health and overall wellness!","xtend5",3);
+VALUES 
+('Momentum 10x', 'http://bit.ly/momentum_logo', 'http://bit.ly/banner_yellow','We"re a brand commited with the health and overall wellness!','momentum10x', 1),
+('Spero', 'http://bit.ly/spero_logo','http://bit.ly/spero_banner','Spero uses non-GMO, organically-produced hemp harvested','spero',2),
+('XTEND5', 'http://bit.ly/xtend5_logo', 'http://bit.ly/banner_xtend5','We"re a brand commited with the health and overall wellness!','xtend5',3);
 
 INSERT INTO
 products (title, brand, p_image, price, in_stock, p_description, site_id)
 
-VALUES ("Multipurpose Oil 600mg Nano Cbd","Momentum 10X", "http://bit.ly/momentum_oil600mg",160.00, 250, "High quality NANO CBD, feel the natural healing.", 1),
-("Pure CBD 500mg","Spero", "http://bit.ly/spero_purecbd", 59.00, 300, "Pure CBD whole plant NO THC", 2),
-("XTEND5 Royal Oil Isolate 500mg","XTEND5", "http://bit.ly/xtend5_500mg", 89.99, 500, "Isolate CBD, natural, healing XTEND your health with us!", 3);
+VALUES ('Multipurpose Oil 600mg Nano Cbd','Momentum 10X', 'http://bit.ly/momentum_oil600mg',160.00, 250, 'High quality NANO CBD, feel the natural healing.', 1),
+('Pure CBD 500mg','Spero', 'http://bit.ly/spero_purecbd', 59.00, 300, 'Pure CBD whole plant NO THC', 2),
+('XTEND5 Royal Oil Isolate 500mg','XTEND5', 'http://bit.ly/xtend5_500mg', 89.99, 500, 'Isolate CBD, natural, healing XTEND your health with us!', 3);
 
 INSERT INTO
 
 resources (name, link)
 
-VALUES ("Why CBD is great for your health?","http://bit.ly/resource_momentum", 1),
-("Lab Analysis Pure CBD","http://bit.ly/spero_resource", 2),
-("The Eggnog & Ruth's XTEND5 Mint Chocolate CBD Recipe","https://xtend5.com/the-history-of-eggnog/", 3);
+VALUES ('Why CBD is great for your health?','http://bit.ly/resource_momentum', 1),
+('Lab Analysis Pure CBD','http://bit.ly/spero_resource', 2),
+('The Eggnog & Ruth"s XTEND5 Mint Chocolate CBD Recipe','https://xtend5.com/the-history-of-eggnog/', 3);
 
 INSERT INTO 
 site_resources (resource_id, site_id)
@@ -67,11 +68,11 @@ VALUES (
 INSERT INTO 
 orders (p_title, total_paid, status, customer_id)
 VALUES (
-    'Multipurpose Oil Nano CBD', 150.99, "Completed", 1
+    'Multipurpose Oil Nano CBD', 150.99, 'Completed', 1
 ),(
-    'XTEND Royal Oil Isolated 500mg', 99.99, "Pending", 2
+    'XTEND Royal Oil Isolated 500mg', 99.99, 'Pending', 2
 ),(
-    'Pure CBD 500mg', 49.00, "Completed", 3
+    'Pure CBD 500mg', 49.00, 'Completed', 3
 );
 
 COMMIT;
